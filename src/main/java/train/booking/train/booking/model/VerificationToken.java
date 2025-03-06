523398674075c9622e38d0ae131ed2dc88c3edab
Marvis <marvis.alimhiantare@gmail.com>
2025-03-06 13:50:08 +0100

    added VerificationToken entity to the model

---
+++ src/main/java/train/booking/train/booking/model/VerificationToken.java
@@ -0,0 +1,33 @@
+package train.booking.train.booking.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "verification_tokens")
+public class VerificationToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime expiresAt;
+}
