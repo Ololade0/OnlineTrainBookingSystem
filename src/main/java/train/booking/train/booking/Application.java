5eeee72a3cc98bf10b6ec3ce4785406b9a66e14e
Ololade0 <ololade.adesuyi@cicod.com>
2025-07-28 13:13:28 +0100

    rendering template

--- src/main/java/train/booking/train/booking/Application.java
+++ src/main/java/train/booking/train/booking/Application.java
@@ -2,14 +2,13 @@ package train.booking.train.booking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJms
 @EnableScheduling
-@PropertySource(value = {"file:/data/TrainBooking/application.properties"})
+//@PropertySource(value = {"file:/data/TrainBooking/application.properties"})
 public class Application {
 
 
