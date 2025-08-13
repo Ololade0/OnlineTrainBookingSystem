1b88a0fcc6f487f2ac5901bedbb95ee7483ab5f0
Ololade <adesuyiololade@gmail.com>
2025-08-13 00:36:31 -0700

    chnaging home controller mapping

--- src/main/java/train/booking/train/booking/Application.java
+++ src/main/java/train/booking/train/booking/Application.java
@@ -9,9 +9,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableJms
 @EnableScheduling
-//@PropertySource(value = {"file:/data/TrainBooking/application.properties"})
+//@PropertySource(value = {"file:/data/TrainBookingLocal/application.properties"})
 public class Application {
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
--- src/main/java/train/booking/train/booking/controller/HomePageController.java
+++ src/main/java/train/booking/train/booking/controller/HomePageController.java
@@ -5,10 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/api/v1/auth/home")
 public class HomePageController {
 
-    @GetMapping("/home-page")
+    @GetMapping("/")
     public String homePage()
     {
         return "home-page";
